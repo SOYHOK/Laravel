e5134061e16386ac11a04be9ddaae1457b44cd35
soy hok <soyhok2018@gmail.com>
2023-05-23 16:55:13 +0700

    View Contact

---
+++ first-project/resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+<header>
+        <nav class="bg-red-500 w-60% h-14">
+           <ul  class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+               <li><a href="/home" class="flex items-center text-white">Home</a></li>
+               <li><a href="/about" class="flex items-center text-white">About</a></li>
+               <li><a href="/contact" class="flex items-center text-white">Contact</a></li> 
+               <li><a href="/login" class="flex items-center text-white" >Login</a></li>
+            <li><a href="/register" class="flex items-center text-white">Register</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <h1 class="text-3xl font-bold text-center pt-10 text-blue-700">Contact Page!</h1>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+</body>
+</html>
